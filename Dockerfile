a10626e3f2667ed4bfd0c451695e4969d8d8d0b6
AlexBaily <alex.d.baily@gmail.com>
2021-05-24 21:00:39 +0000

    Add dockerfile, splash.jpg and package lock

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
-FROM mhart/alpine-node:11 AS builder
+FROM mhart/alpine-node:11 as builder
 WORKDIR /app
 COPY . .
-RUN yarn run build
+
+ARG REACT_APP_AUDIENCE
+ARG REACT_APP_DOMAIN
+ARG REACT_APP_CLIENTID
+
+ENV REACT_APP_AUDIENCE=$REACT_APP_AUDIENCE
+ENV REACT_APP_DOMAIN=$REACT_APP_DOMAIN
+ENV REACT_APP_CLIENTID=$REACT_APP_CLIENTID
+
+RUN npm install react-scripts -g --silent
+RUN npm install
+RUN npm run build
 
 FROM mhart/alpine-node
 RUN yarn global add serve
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./utils/react-auth0-spa";
-import config from "./auth_config.json";
 import history from "./utils/history";
 
 // A function that routes the user to the right place
@@ -18,10 +17,10 @@ const onRedirectCallback = appState => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.domain}
-    client_id={process.env.clientId}
+    domain={process.env.REACT_APP_DOMAIN}
+    client_id={process.env.REACT_APP_CLIENTID}
     redirect_uri={window.location.origin}
-    audience={process.env.audience}
+    audience={process.env.REACT_APP_AUDIENCE}
     onRedirectCallback={onRedirectCallback}
   >
     <App />
